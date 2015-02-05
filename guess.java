52ee92fd20fa3e2a424f25e6c3bcbfe23e7968af
zarochi <fiesam19@uww.edu>
2015-02-05 16:25:48 -0600

    Revert "Version 1.1"
    This reverts commit 0e08804effb6d96e80c34c3c7b65edbd8b320a8e.

--- guess.java
+++ guess.java
@@ -5,22 +5,17 @@ public class guess
    public int number;
    public guess()
    {
-      number=(int)(Math.random()*10);
+      number=(int)(Math.random()*100);
    }
    
    public static void main(String[] args)
    {
       guess g = new guess();
-      boolean incorrect=true;
-      System.out.println("Please enter a number between 1 and 10 to guess: ");
-      while (incorrect)
-      {
-         int user = Keyboard.readInt();
-         if (user==g.number)
-            incorrect=false;
-         else
-            System.out.println("Incorrect, please guess again");
-      }
-      System.out.println("Congrats; "+g.number+" was correct.");
+      System.out.println("Please enter a number between 1 and 100 to guess: ");
+      int user = Keyboard.readInt();
+      if (user==g.number)
+         System.out.println("Correct");
+      else
+         System.out.println("Incorrect; the answer is: "+g.number);
    }
 }
