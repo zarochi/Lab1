30b2e75e69c37fb27ee91b4a9e51327d484be01b
zarochi <fiesam19@uww.edu>
2015-02-05 16:37:55 -0600

    v1.1

--- guess.java
+++ guess.java
@@ -5,17 +5,22 @@ public class guess
    public int number;
    public guess()
    {
-      number=(int)(Math.random()*100);
+      number=(int)(Math.random()*10);
    }
    
    public static void main(String[] args)
    {
       guess g = new guess();
-      System.out.println("Please enter a number between 1 and 100 to guess: ");
-      int user = Keyboard.readInt();
-      if (user==g.number)
-         System.out.println("Correct");
-      else
-         System.out.println("Incorrect; the answer is: "+g.number);
+      boolean incorrect=true;
+      System.out.println("Please enter a number between 1 and 10 to guess: ");
+      while (incorrect)
+      {
+         int user = Keyboard.readInt();
+         if (user==g.number)
+            incorrect=false;
+         else
+            System.out.println("Incorrect, please guess again");
+      }
+      System.out.println("Congrats; "+g.number+" was correct.");
    }
 }
